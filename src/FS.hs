07982c9ee264d10cac7a60ed3b85c2fae2b07e10
klenkiew <k.lenkiewicz@protonmail.com>
2019-11-03 17:21:14 +0100

    Remove commented out code

--- src/FS.hs
+++ src/FS.hs
@@ -8,11 +8,8 @@ module FS
 
 import Data.List (find)
 import Data.Strings (strSplit)
-import Debug.Trace (trace)
 
 skip :: Char -> String -> String
---skip (f:rest) toSkip
---  | f == toSkip = rest
 skip toSkip string = case string of
   toSkip:rest -> rest
 
@@ -20,12 +17,10 @@ data FSObject = Directory { name :: String, directoryContent :: [FSObject] }
                 | File { name :: String, fileContent :: String } deriving (Show)
 
 getFSObject :: FSObject -> String -> Maybe FSObject
---getFSObject file path | trace ("getFSObject " ++ show file ++ " " ++ show path) False = undefined
 getFSObject (Directory _ content) fsObjectName = find ((== fsObjectName) . name) content
 getFSObject _ _ = Nothing
 
 getFSObjectByPath :: FSObject -> String -> Maybe FSObject
---getFSObjectByPath file path | trace ("getFSObjectByPath " ++ show file ++ " " ++ show path) False = undefined
 getFSObjectByPath dir@(Directory _ _) path = getFSObject dir fsObjectName >>= flip getFSObjectByPath restPath
   where (fsObjectName, restPath) = strSplit "/" path
 getFSObjectByPath file@File{} "" = Just file
@@ -34,10 +29,7 @@ getFSObjectByPath _ _ = Nothing
 
 class FileSystem a where
   getObject :: a -> String -> Maybe FSObject
---  createFile :: a -> String -> a
---  createDirectory :: a -> String -> a
 
 newtype MemoryFileSystem = MemoryFileSystem { rootDirectory :: FSObject }
 instance FileSystem MemoryFileSystem where
-  getObject (MemoryFileSystem rootDirectory) = getFSObjectByPath rootDirectory . skip '/' 
-
+  getObject (MemoryFileSystem rootDirectory) = getFSObjectByPath rootDirectory . skip '/'
